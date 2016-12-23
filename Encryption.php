8530f8e0f66a2ed1431d968be59c7a3492d3489d
liandro123 <liandro123@live.com>
2016-12-23 18:58:23 -0500

    Additions
    Added more encryption with random characters and numbers. Added ability to check if password is same as previously encrypted word. Gives response if correct or not.

--- Encryption.php
+++ Encryption.php
@@ -1,14 +1,33 @@
 <html>
 <?php
-$word = $_GET['word'];
+function encryp (){
+$word = $_GET['w'];
+$hex ='';
+ for ($i=0; $i<strlen($word); $i++){
+        $nw = ord($word[$i]);
+        $hexCode = dechex($nw);
+        $hex .= '0'.$hexCode;
+        }
 
-$nw = dechex($word);
+        for ($i=0;$i<strlen($hex);$i++){
+        $hexar =strval($hex);
+        $hexar = str_split($hexar, 1);
 
-echo $nw;
-
-
-
-
-
- ?>
+        }
+        for ($i=1;$i<strlen($hex);$i++){
+         $s = implode("s4",$hexar);
+        $a = str_rot13("abcdefghijklmnopqrstuvwxyz1234567890"). strtoupper(str_rot13("abcdefghijklmnopqrstuvwxyz1234567890"));
+         $s = substr_replace ($s, "b02lasakso2io89asnfa" , $i,$i);
+        }
+        echo($s."\n");
+        return $s;
+        }
+        $as = encryp();
+$p = $_GET['p'];
+if ($p === $as) {
+echo "OKAY!";
+}else{
+echo "NOT OKAY!";
+}
+?>
 </html>
